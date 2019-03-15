425925847cd39cf6a418a517889edad50e00fbca
wanching_97 <wanching_97@hotmail.com>
2019-03-15 12:57:36 +0800

    successful hosting of db and express

--- express/src/instances/sequelize.ts
+++ express/src/instances/sequelize.ts
@@ -4,29 +4,29 @@ const db = 'subscription';
 const username = 'root';
 const password = '12345678';
 
-//export const sequelize = new Sequelize.Sequelize('mysql://root:12345678@newsletter-subscription.crxlxcw54ii4.us-east-2.rds.amazonaws.com:3306/subscription',{});
-export const sequelize = new Sequelize.Sequelize(db, username, password, {
-  host: 'newsletter-subscription.crxlxcw54ii4.us-east-2.rds.amazonaws.com',
-  dialect: "mysql",
-  port: 3306,
-  retry: {
-    match: [
-      /ETIMEDOUT/,
-			/EHOSTUNREACH/,
-			/ECONNRESET/,
-			/ECONNREFUSED/,
-			/ETIMEDOUT/,
-			/ESOCKETTIMEDOUT/,
-			/EHOSTUNREACH/,
-			/EPIPE/,
-			/EAI_AGAIN/,
-			/SequelizeConnectionError/,
-			/SequelizeConnectionRefusedError/,
-			/SequelizeHostNotFoundError/,
-			/SequelizeHostNotReachableError/,
-			/SequelizeInvalidConnectionError/,
-			/SequelizeConnectionTimedOutError/
-    ],
-    max: 5
-  }
-});
+export const sequelize = new Sequelize.Sequelize('mysql://root:12345678@newsletter-subscription.coyzmr8jvwy0.ap-southeast-1.rds.amazonaws.com:3306/subscription',{});
+// export const sequelize = new Sequelize.Sequelize(db, username, password, {
+//   host: 'newsletter-subscription.crxlxcw54ii4.us-east-2.rds.amazonaws.com',
+//   dialect: "mysql",
+//   port: 3306,
+//   retry: {
+//     match: [
+//       /ETIMEDOUT/,
+// 			/EHOSTUNREACH/,
+// 			/ECONNRESET/,
+// 			/ECONNREFUSED/,
+// 			/ETIMEDOUT/,
+// 			/ESOCKETTIMEDOUT/,
+// 			/EHOSTUNREACH/,
+// 			/EPIPE/,
+// 			/EAI_AGAIN/,
+// 			/SequelizeConnectionError/,
+// 			/SequelizeConnectionRefusedError/,
+// 			/SequelizeHostNotFoundError/,
+// 			/SequelizeHostNotReachableError/,
+// 			/SequelizeInvalidConnectionError/,
+// 			/SequelizeConnectionTimedOutError/
+//     ],
+//     max: 5
+//   }
+// });
